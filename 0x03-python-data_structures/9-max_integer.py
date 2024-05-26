072fba1c4165c96ea6f1b3499d919b8e85c87007
aylaaoufi <ayoub.laaoufi@hotmail.com>
2024-05-26 18:24:13 +0100

    print big integer

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list == 0:
+        return "None"
+    else:
+        max_n = my_list[0]
+        for i in my_list:
+            if i > max_n:
+                max_n = i
+        return max_n
