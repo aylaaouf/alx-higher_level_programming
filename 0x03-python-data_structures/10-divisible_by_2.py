92ae78d9fd78cf0a2a6f44ef192f81371b0e2a2c
aylaaoufi <ayoub.laaoufi@hotmail.com>
2024-06-14 03:39:34 +0100

    fix style

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     list2 = []
     for i in my_list:
-        if i % 2==0:
+        if i % 2 == 0:
             list2.append(True)
         else:
             list2.append(False)
