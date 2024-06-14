c5901b6310f54c753b5eeb49d26074cadec3887b
aylaaoufi <ayoub.laaoufi@hotmail.com>
2024-06-14 03:58:06 +0100

    last task

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    if not a_dictionary:
+    if a_dictionary is None:
         return None
     new = {}
-    for i, j in a_dictionary.items():
-        if j % 2 == 0:
-            new[i] = j * 2
-        else:
-            new[i] = j
+    for key, value in a_dictionary.items():
+        if value % 2 == 0:
+            new[key] = value * 2
     return new
