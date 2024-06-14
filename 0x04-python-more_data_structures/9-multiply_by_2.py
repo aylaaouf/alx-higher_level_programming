19fc9a417e932694ab2a2f72d06dbb3f25a770b3
aylaaoufi <ayoub.laaoufi@hotmail.com>
2024-06-14 03:52:16 +0100

    op

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,7 +3,7 @@
 def multiply_by_2(a_dictionary):
     new = {}
     for i, j in a_dictionary.items():
-        if a_dictionary[i] % 2 == 0:
+        if j % 2 == 0:
             new[i] = j * 2
         else:
             new[i] = j
