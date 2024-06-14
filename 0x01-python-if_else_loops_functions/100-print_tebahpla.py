a3acaab2205ed76a95f829f89efbd14a4d2d1130
aylaaoufi <ayoub.laaoufi@hotmail.com>
2024-06-14 03:37:57 +0100

    checker fu** me hard

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for x in range(122, 96, -1):
-    output = ""
-    output += chr(x) if x % 2 == 0 else chr(x - 32)
-print(output)
+for x in range(26):
+    if x % 2 == 0:
+        co = 32
+    else:
+        co = 0
+    print("{}".format(chr((26 - x + 64) + co)), end="")
