18130178e6a650667f6a388463880a0bb682716a
aylaaoufi <116092909+youssef3092004@users.noreply.github.com>
2024-06-18 21:48:39 +0300

    js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const process = require('process');
-x == process.argv[2];
-y == process.argv[3];
+const x = process.argv[2];
+const y = process.argv[3];
 console.log(x + " is " + y);
