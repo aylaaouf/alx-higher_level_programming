ef98d6067f9ef4755dac3c7a55b15ca765d61ec7
aylaaoufi <116092909+youssef3092004@users.noreply.github.com>
2024-06-18 22:20:11 +0300

    js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,8 +2,6 @@
 const process = require('process');
 if (!process.argv[2] && !process.argv[3]) {
   console.log('undefined is undefined');
-} else if (!process.argv[2] && process.argv[3]) {
-  console.log('undefined is ' + process.argv[3]);
 } else if (process.argv[2] && !process.argv[3]) {
   console.log(process.argv[2] + ' is undefined');
 } else {
