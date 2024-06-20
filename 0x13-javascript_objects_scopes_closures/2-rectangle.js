ce8d6ef3ac6745ef9c83626e2e3f82ff38861852
aylaaoufi <ayoub.laaoufi@hotmail.com>
2024-06-20 11:14:21 -0700

    js

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+    if (width || height) {
+      const obj = {};
+    }
+  }
+}
+module.exports = Rectangle;
