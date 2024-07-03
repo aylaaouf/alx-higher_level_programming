0cfcf59ee5bdfdccf359355874256b9b71716b25
aylaaoufi <ayoub.laaoufi@hotmail.com>
2024-07-02 21:43:43 -0700

    db

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,5 @@
-    -- create teh table called first table and has two attribute id and name
-    CREATE TABLE IF NOT EXISTS first_table(
-        id INT,
+-- create teh table called first table and has two attribute id and name
+CREATE TABLE IF NOT EXISTS first_table(
+	id INT,
         name VARCHAR(256)
-    );
-
+);
